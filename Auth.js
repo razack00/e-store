4b3be3b62c62bf0c1de2c36498992872cdfbcdb1
razack00 <121952296+razack00@users.noreply.github.com>
2023-06-09 23:39:46 +0100

    mobile nav toggle

--- Auth.js
+++ Auth.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const storeTextHTMLTag = document.querySelector(".user");
-    const dropdown = document.querySelector(".dropdown");
+  const storeTextHTMLTag = document.querySelector(".user");
+  const dropdown = document.querySelector(".dropdown");
+  const register = document. querySelector('.register')
 
   const user = localStorage.getItem("user");
   const username = JSON.parse(user);
   if (!localStorage.getItem("isLoggedIn")) {
     storeTextHTMLTag.innerHTML = `<button class="loginlogout">Log In</button>`;
+    register.style.display = 'block'
     storeTextHTMLTag.onclick = () => {
       location.href = "signin.html";
     };
   } else {
       const logoutBtn = document.querySelector(".loginlogout");
+      register.style.display = 'none'
       storeTextHTMLTag.textContent = `${username.FirstName}`;
 
       logoutBtn.onclick = () => { 
@@ -29,6 +32,21 @@ document.addEventListener("DOMContentLoaded", () => {
         //   }
           
           dropdown.style.display =  dropdown.style.display === "flex" ? "none" : "flex";
+      }
+  }
+
+
+  //add logic for toggling the menu
+  const menu = document.querySelector(".mobile-nav");
+  const menuToggle = document.querySelector(".menu-btn");
+  menuToggle.onclick = () => {
+      menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+  };
+
+  //hide the menu on bigger screens
+  window.onresize = () => {
+      if (window.innerWidth > 548) {
+          menu.style.display = "none"
       }
   }
 });
